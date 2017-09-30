9283e118f59a563ae5917c2593bea49c86bb0d65
feychou <federica.recanatini@gmail.com>
2017-09-30 16:36:17 +0200

    Split application in multiple components

---
+++ src/components/Game/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Portrait from '../Portrait';
+import Selectables from '../Selectables';
+import './style.css';
+
+const Game = () => (
+  <div className="Game">
+    <Portrait />
+    <Selectables />
+  </div>
+);
+
+export default Game;
---
+++ src/components/Selectables/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.css';
+
+const Selectables = () => (
+  <div className="Selectables">
+    <div className="Nav">
+    </div>
+    <div className="Panel">
+    </div>
+  </div>
+);
+
+export default Selectables;
