2888aef3d9efa15bda276d88b6969ac36e79fc1a
feychou <federica.recanatini@gmail.com>
2017-10-12 21:17:01 +0200

    Reposition ballgag

--- src/game/components/toys/Ballgag/index.js
+++ src/game/components/toys/Ballgag/index.js
@@ -3,41 +3,36 @@ import React from 'react';
 import './style.css';
 
 const Ballgag = () => (
-  <svg className="Ballgag" xmlns='http://www.w3.org/2000/svg' viewBox='0 -300.375 1169.547 1092.375'>
-    <g id='Gag'>
-      <path fill='none' stroke='#A7A5A6' strokeWidth='5' strokeMiterlimit='10' d='M239.512,265.793l0.702,12.26 c0.641,11.202-7.914,20.796-19.116,21.438l-8.216,0.47l-3.024-52.813l8.217-0.471C229.276,246.036,238.871,254.591,239.512,265.793 z' />
-      <path fill='none' stroke='#A7A5A6' strokeWidth='5' strokeMiterlimit='10' d='M345.592,259.718l0.702,12.26 c0.641,11.202,10.236,19.757,21.438,19.116l8.216-0.471l-3.024-52.813l-8.216,0.47 C353.506,238.921,344.951,248.516,345.592,259.718z' />
-      <path fill='gray' stroke='#000' strokeWidth='3' strokeMiterlimit='10' d='M219.39,293.97L51,303.33 c0.227-0.85,0.453-1.69,0.68-2.53c1.792-6.66,3.604-13.26,5.458-19.78c1.926-6.78,3.882-13.48,5.88-20.1l153.922-8.56 L219.39,293.97z' />
-      <path fill='gray' stroke='#000' strokeWidth='3' strokeMiterlimit='10' d='M555,275.01L369.041,285.4l-2.451-41.61 l173.388-9.68c1.077,2.69,2.153,5.39,3.209,8.12c2.399,6.16,4.727,12.43,6.993,18.8C551.821,265.64,553.421,270.3,555,275.01z' />
-      <rect x='232.237' y='257.44' transform='rotate(-3.279 292.208 268.743)' fill='gray' stroke='#000' strokeWidth='3' strokeMiterlimit='10' width='120.333' height='22.947' />
-      <g fill='none' stroke='#000' strokeWidth='2' strokeMiterlimit='10'>
-        <line x1='232.001' y1='264.503' x2='234.496' y2='264.351' />
-        <line strokeDasharray='5.012,5.012' x1='239.499' y1='264.048' x2='347.06' y2='257.519' />
-        <line x1='349.561' y1='257.367' x2='352.057' y2='257.215' />
+  <svg className="Ballgag" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 508.03 72.39">
+    <title>Element 7</title>
+    <g id="Ebene_2" dataname="Ebene 2">
+      <g id="Gag">
+        <path d="M190.49,33.24l.7,12.26a20.31,20.31,0,0,1-19.12,21.44l-8.22.47-3-52.81,8.22-.47A20.31,20.31,0,0,1,190.49,33.24Z" fill="none" stroke="#a7a5a6" strokeMiterlimit="10" strokeWidth="5"/>
+        <path d="M296.57,27.17l.7,12.26a20.31,20.31,0,0,0,21.44,19.12l8.22-.47-3-52.81-8.22.47A20.31,20.31,0,0,0,296.57,27.17Z" fill="none" stroke="#a7a5a6" strokeMiterlimit="10" strokeWidth="5"/>
+        <path d="M170.37,61.42,2,70.78l.68-2.53q2.69-10,5.46-19.78Q11,38.3,14,28.37l153.92-8.56Z" fill="gray" stroke="#000" strokeMiterlimit="10" strokeWidth="3"/>
+        <path d="M506,42.46,320,52.85l-2.45-41.61L491,1.56c1.08,2.69,2.15,5.39,3.21,8.12q3.6,9.24,7,18.8Q503.62,35.39,506,42.46Z" fill="gray" stroke="#000" strokeMiterlimit="10" strokeWidth="3"/>
+        <rect x="183.22" y="24.89" width="120.33" height="22.95" transform="translate(-1.68 13.97) rotate(-3.28)" fill="gray" stroke="#000" strokeMiterlimit="10" strokeWidth="3"/>
+        <line x1="182.98" y1="31.95" x2="185.48" y2="31.8" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
+        <line x1="190.48" y1="31.5" x2="298.04" y2="24.97" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="5.01 5.01"/>
+        <line x1="300.54" y1="24.81" x2="303.04" y2="24.66" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
+        <line x1="182.98" y1="47.94" x2="185.48" y2="47.78" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
+        <line x1="190.48" y1="47.48" x2="298.04" y2="40.95" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="5.01 5.01"/>
+        <line x1="300.54" y1="40.8" x2="303.04" y2="40.65" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
+        <circle cx="242.72" cy="36.4" r="27.99" transform="translate(-1.68 13.93) rotate(-3.28)" fill="#ff3939" stroke="red" strokeMiterlimit="10" strokeWidth="5"/>
+        <circle cx="144.05" cy="42.05" r="6.89" transform="translate(-2.17 8.3) rotate(-3.28)" fill="#dbdada"/>
+        <circle cx="339.73" cy="30.84" r="6.89" transform="matrix(1, -0.06, 0.06, 1, -1.21, 19.47)" fill="#dbdada"/>
+        <circle cx="285.51" cy="33.95" r="3.71" transform="matrix(1, -0.06, 0.06, 1, -1.47, 16.37)" fill="#dbdada"/>
+        <circle cx="198.83" cy="38.91" r="3.71" transform="translate(-1.9 11.43) rotate(-3.28)" fill="#dbdada"/>
+        <line x1="16.69" y1="34.66" x2="16.69" y2="34.66" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="3"/>
+        <line x1="12.68" y1="32.76" x2="16.18" y2="32.58" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
+        <line x1="23.25" y1="32.21" x2="161.18" y2="25.06" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="7.08 7.08"/>
+        <line x1="164.71" y1="24.88" x2="168.21" y2="24.7" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
+        <line x1="3.2" y1="66.26" x2="6.69" y2="66.07" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
+        <line x1="13.65" y1="65.69" x2="163.15" y2="57.58" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="6.96 6.96"/>
+        <line x1="166.63" y1="57.39" x2="170.12" y2="57.2" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
+        <line x1="317.86" y1="16.1" x2="493.02" y2="6.73" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="7"/>
+        <line x1="319.79" y1="48.96" x2="504.39" y2="37.74" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="7"/>
       </g>
-      <g fill='none' stroke='#000' strokeWidth='2' strokeMiterlimit='10'>
-        <line x1='232.001' y1='280.489' x2='234.496' y2='280.337' />
-        <line strokeDasharray='5.012,5.012' x1='239.499' y1='280.034' x2='347.06' y2='273.505' />
-        <line x1='349.561' y1='273.353' x2='352.057' y2='273.201' />
-      </g>
-      <ellipse transform='rotate(-3.279 291.548 268.785)' fill='#FF3939' stroke='#F00' strokeWidth='5' strokeMiterlimit='10' cx='291.737' cy='268.951' rx='27.985' ry='27.985' />
-      <ellipse transform='rotate(-3.279 192.924 274.408)' fill='#DBDADA' cx='193.067' cy='274.601' rx='6.895' ry='6.895' />
-      <ellipse transform='rotate(-3.279 388.514 263.248)' fill='#DBDADA' cx='388.746' cy='263.397' rx='6.895' ry='6.895' />
-      <ellipse transform='rotate(-3.279 334.283 266.321)' fill='#DBDADA' cx='334.524' cy='266.503' rx='3.709' ry='3.709' />
-      <ellipse transform='rotate(-3.279 247.732 271.34)' fill='#DBDADA' cx='247.852' cy='271.465' rx='3.709' ry='3.709' />
-      <line fill='none' stroke='#000' strokeWidth='3' strokeMiterlimit='10' x1='65.705' y1='267.215' x2='65.705' y2='267.215' />
-      <g fill='none' stroke='#000' strokeWidth='2' strokeMiterlimit='10'>
-        <line x1='61.702' y1='265.31' x2='65.197' y2='265.129' />
-        <line strokeDasharray='7.0826,7.0826' x1='72.27' y1='264.763' x2='210.195' y2='257.616' />
-        <line x1='213.732' y1='257.432' x2='217.227' y2='257.251' />
-      </g>
-      <g fill='none' stroke='#000' strokeWidth='2' strokeMiterlimit='10'>
-        <line x1='52.217' y1='298.809' x2='55.712' y2='298.619' />
-        <line strokeDasharray='6.9639,6.9639' x1='62.665' y1='298.242' x2='212.17' y2='290.134' />
-        <line x1='215.647' y1='289.946' x2='219.142' y2='289.756' />
-      </g>
-      <line fill='none' stroke='#000' strokeWidth='2' strokeMiterlimit='10' strokeDasharray='7' x1='366.876' y1='248.648' x2='542.035' y2='239.282' />
-      <line fill='none' stroke='#000' strokeWidth='2' strokeMiterlimit='10' strokeDasharray='7' x1='368.812' y1='281.515' x2='553.405' y2='270.292' />
     </g>
   </svg>
 );
