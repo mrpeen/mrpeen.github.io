7b4a79d8dd02535ce9787a59750e6c8ae0563c19
feychou <federica.recanatini@gmail.com>
2017-10-15 15:16:12 +0200

    Fix mask and wand

--- src/game/components/toys/Ballgag/index.js
+++ src/game/components/toys/Ballgag/index.js
@@ -13,10 +13,10 @@ const Ballgag = () => (
         <path d="M506,42.46,320,52.85l-2.45-41.61L491,1.56c1.08,2.69,2.15,5.39,3.21,8.12q3.6,9.24,7,18.8Q503.62,35.39,506,42.46Z" fill="gray" stroke="#000" strokeMiterlimit="10" strokeWidth="3"/>
         <rect x="183.22" y="24.89" width="120.33" height="22.95" transform="translate(-1.68 13.97) rotate(-3.28)" fill="gray" stroke="#000" strokeMiterlimit="10" strokeWidth="3"/>
         <line x1="182.98" y1="31.95" x2="185.48" y2="31.8" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
-        <line x1="190.48" y1="31.5" x2="298.04" y2="24.97" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="5.01 5.01"/>
+        <line x1="190.48" y1="31.5" x2="298.04" y2="24.97" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" strokeDasharray="5.01 5.01"/>
         <line x1="300.54" y1="24.81" x2="303.04" y2="24.66" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
         <line x1="182.98" y1="47.94" x2="185.48" y2="47.78" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
-        <line x1="190.48" y1="47.48" x2="298.04" y2="40.95" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="5.01 5.01"/>
+        <line x1="190.48" y1="47.48" x2="298.04" y2="40.95" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" strokeDasharray="5.01 5.01"/>
         <line x1="300.54" y1="40.8" x2="303.04" y2="40.65" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
         <circle cx="242.72" cy="36.4" r="27.99" transform="translate(-1.68 13.93) rotate(-3.28)" fill="#ff3939" stroke="red" strokeMiterlimit="10" strokeWidth="5"/>
         <circle cx="144.05" cy="42.05" r="6.89" transform="translate(-2.17 8.3) rotate(-3.28)" fill="#dbdada"/>
@@ -25,13 +25,13 @@ const Ballgag = () => (
         <circle cx="198.83" cy="38.91" r="3.71" transform="translate(-1.9 11.43) rotate(-3.28)" fill="#dbdada"/>
         <line x1="16.69" y1="34.66" x2="16.69" y2="34.66" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="3"/>
         <line x1="12.68" y1="32.76" x2="16.18" y2="32.58" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
-        <line x1="23.25" y1="32.21" x2="161.18" y2="25.06" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="7.08 7.08"/>
+        <line x1="23.25" y1="32.21" x2="161.18" y2="25.06" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" strokeDasharray="7.08 7.08"/>
         <line x1="164.71" y1="24.88" x2="168.21" y2="24.7" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
         <line x1="3.2" y1="66.26" x2="6.69" y2="66.07" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
-        <line x1="13.65" y1="65.69" x2="163.15" y2="57.58" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="6.96 6.96"/>
+        <line x1="13.65" y1="65.69" x2="163.15" y2="57.58" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" strokeDasharray="6.96 6.96"/>
         <line x1="166.63" y1="57.39" x2="170.12" y2="57.2" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2"/>
-        <line x1="317.86" y1="16.1" x2="493.02" y2="6.73" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="7"/>
-        <line x1="319.79" y1="48.96" x2="504.39" y2="37.74" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" stroke-dasharray="7"/>
+        <line x1="317.86" y1="16.1" x2="493.02" y2="6.73" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" strokeDasharray="7"/>
+        <line x1="319.79" y1="48.96" x2="504.39" y2="37.74" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="2" strokeDasharray="7"/>
       </g>
     </g>
   </svg>
