43f4905f2ea0c7d855fcf3e96289603d8c2d1ff5
feychou <federica.recanatini@gmail.com>
2017-10-17 20:46:37 +0200

    Cleanup Panel

--- src/game/components/Panel/component.js
+++ src/game/components/Panel/component.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Circle from '../Circle';
 import SkintonePanel from '../SkintonePanel';
 import MakeUpPanel from '../MakeUpPanel';
 import ToysPanel from '../ToysPanel';
@@ -8,47 +7,8 @@ import ExtrasPanel from '../ExtrasPanel';
 import BackgroundPanel from '../BackgroundPanel';
 import './style.css';
 
-const getItem = (
-  {type, fill, id, clears, icon, name},
-  selected,
-  active,
-  onClickToy,
-  onClickColor,
-  onClickBackground) => {
-  switch(type) {
-    case 'color':
-      return (<Circle
-                id={id}
-                fill={fill}
-                selected={selected} 
-                onClick={onClickColor} />)
-    case 'icon':
-      return (<span
-                className="Wearable"
-                key={id}
-                onClick={() => onClickToy(id, active, clears)}>
-                <img src={icon} alt={name} />
-              </span>)
-    default: 
-      return (<Circle
-                id={id}
-                background={icon}
-                onClick={onClickBackground} />)
-  }
-}
-
-const Panel = ({
-  selected,
-  items,
-  active,
-  onClickColor,
-  onClickToy,
-  onClickRemoveToys,
-  toClear,
-  onClickBackground
-}) => (
+const Panel = ({selected}) => (
   <div className="Panel">
-
     {selected === 'skintone' && <SkintonePanel />}
 
     {selected === 'makeUp' && <MakeUpPanel />}
@@ -58,36 +18,6 @@ const Panel = ({
     {selected === 'extras' && <ExtrasPanel />}
 
     {selected === 'background' && <BackgroundPanel />}
-    {/* {items.map((item) => 
-      <div
-        key={item.id}
-        className="Panel--Item"> 
-          {getItem(item, selected, active, onClickToy, onClickColor, onClickBackground)}
-      </div>)}
-
-      {toClear && selected === 'condom' &&
-        <div className="Panel--Item">
-          <Circle
-            isReset={true}
-            selected={selected}
-            onClick={() => onClickColor(null, selected)} />
-        </div>}
-
-      {toClear && selected === 'toys' &&
-        <div className="Panel--Item">
-          <Circle
-            isReset={true}
-            selected={selected}
-            onClick={onClickRemoveToys} />
-        </div>}
-
-      {toClear && selected === 'background' &&
-        <div className="Panel--Item">
-          <Circle
-            isReset={true}
-            selected={selected}
-            onClick={() => onClickBackground(null)} />
-        </div>} */}
   </div>
 );
 
