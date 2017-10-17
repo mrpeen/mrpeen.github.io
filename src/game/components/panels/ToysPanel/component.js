738ecfc6e5974325a5536356bce9a6ea0430a440
feychou <federica.recanatini@gmail.com>
2017-10-17 21:27:45 +0200

    Create PanelItemIcon

--- src/game/components/panels/ToysPanel/component.js
+++ src/game/components/panels/ToysPanel/component.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 import Circle from '../../Circle';
 import PanelItem from '../atoms/PanelItem';
+import PanelItemIcon from '../atoms/PanelItemIcon';
 
 const ToysPanel = ({items, active, onClick, onClickClear}) => (
   <div className="ToysPanel">
     {items.map(({id, clears, icon, name}) => 
       <PanelItem key={id}>
-        <span
-          className="ItemIcon"
+        <PanelItemIcon
           key={id}
-          onClick={() => onClick(id, active, clears)}>
-          <img src={icon} alt={name} />
-        </span>
+          onClick={() => onClick(id, active, clears)}
+          icon={icon}
+          name={name} />
       </PanelItem>)}
 
     <PanelItem>
