4df58b9c470a36a1e62fe43c2b7aad590c43d7a2
feychou <federica.recanatini@gmail.com>
2017-11-06 21:28:41 +0100

    Restore exception for FF

--- src/game/components/UserActions/component.js
+++ src/game/components/UserActions/component.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import FileSaver from 'file-saver';
+import domtoimage from 'dom-to-image';
 import html2canvas from 'html2canvas';
 
 import withWindowResize from '../../containers/withWindowResize';
 import './style.css';
 
+const isFF = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
 const onExport = () => {
-  html2canvas(document.getElementById('Photo'), {
-    onrendered: (canvas) => {
-      canvas.toBlob(blob => FileSaver.saveAs(blob, 'mr-peen.png'));
-    }
-});
+  if (isFF) {
+    domtoimage.toBlob(document.getElementById('Photo'))
+      .then(blob => {
+        FileSaver.saveAs(blob, 'mr-peen.png');
+      });
+  } else {
+    html2canvas(document.getElementById('Photo'), {
+      onrendered: (canvas) => {
+        canvas.toBlob(blob => FileSaver.saveAs(blob, 'mr-peen.png'));
+      }
+    });
+  }
 }
 
 const UserActions = ({onClickReset, windowWidth}) => (
