d2874090c7a1f655716c2681759535274811b058
feychou <federica.recanatini@gmail.com>
2017-11-11 17:36:16 +0100

    Enable hash history

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'font-awesome-webpack';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
 
