f6633ef7e37cc3334b0c7b6ad944724d4df3f7bc
feychou <federica.recanatini@gmail.com>
2017-11-11 17:17:46 +0100

    Add mara

--- src/website/pages/About/index.js
+++ src/website/pages/About/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Game from '../../../game';
 import Fey from '../../static/fey.jpg';
+import Mara from '../../static/mara.jpg';
 import './style.css';
 
 const About = () => (
@@ -47,10 +48,31 @@ const About = () => (
         You can also show your appreciation and share your favourite runway looks on <a target="_blank" href="https://www.facebook.com/MrHPeen/">Facebook</a>.<br /><br />
 
         These human beings are happy to serve Mr Peen as their personal styling assistants:<br /><br />
+        <div className="TeamMember">
+          <span
+            style={{backgroundImage: `url(${Mara})`, backgroundPosition: '30% 0'}}
+            className="TeamMember--pic" />
+          <span className="TeamMember--description">
+            <b>Mara</b> enjoys creating art, even better when it’s sex positive.
+            Her favorite mediums are film, pictures and rope.
+            A tiny squishy toy mad its way into her heart and she had no choice but to give it live - thus Mr Peen was born!<br />
+            <a
+              className="SocialIcon"
+              target="_blank"
+              href="https://linkedin.com/in/mara-langenberger">
+              <i className="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+            <a 
+              className="SocialIcon"
+              href="mailto:mara.emilia@gmx.net">
+              <i className="fa fa-envelope" aria-hidden="true"></i>
+            </a>
+          </span>
+        </div>
 
         <div className="TeamMember">
           <span
-            style={{backgroundImage: `url(${Fey})`}}
+            style={{backgroundImage: `url(${Fey})`, backgroundPosition: '50% 0'}}
             className="TeamMember--pic" />
           <span className="TeamMember--description">
             <b>Fey</b> likes to code funny things but dislikes most food. She wears anything that's weird enough and has always wanted to make a dress up game.
